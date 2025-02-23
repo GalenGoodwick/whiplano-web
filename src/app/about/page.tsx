a123eee7a2ceef228e246b995bcb79212dfed60b
Adhi742007 <rakeshphal79@gmai.com>
2025-02-23 15:46:02 +0530

    Changed the contents of about us and changed its bg to pink gradient

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function AboutUs() {
       {/* <Header /> */}
         <div className="bg-white">
           <div className="bg-black py-9"></div>
-          <h1 className=" flex flex-col items-center text-6xl font-extrabold my-10 text-[#C81E8A]">About Us</h1>
+          <h1 className=" flex flex-col items-center text-6xl font-extrabold my-10 text-[#C81E8A]">About Whiplano</h1>
           <hr className="w-1/2 border-t-2 border-gray-300 my-4 mx-auto" />
           <div><AboutText/></div>
         </div>
