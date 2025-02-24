732810d806bc2aea94148f129d6f9b18f6470df4
Adhi742007 <rakeshphal79@gmai.com>
2025-02-24 08:49:35 +0530

    Chaged header color while scrolling , from pink to black

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -27,7 +27,8 @@ export default function Header() {
   return (
     <header
       className={`fixed top-0 left-0 w-full z-30 transition-all duration-300 ${
-        isScrolled ? "bg-[#DB2777]" : "bg-transparent"
+        isScrolled ? "bg-black" : "bg-transparent" 
+        // [#DB2777] pink
       }`}
     >
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
